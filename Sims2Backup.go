a2dcd607b89e617d843c4dfd5e00ce9221f41770
lingeringwillx <111698406+lingeringwillx@users.noreply.github.com>
2024-03-28 19:28:59 +0300

    Added option to use custom launcher

--- Sims2Backup.go
+++ Sims2Backup.go
@@ -5,12 +5,13 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
-	"strconv"
-	"strings"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"slices"
+	"syscall"
 	"time"
 	
 	"golang.org/x/sys/windows/registry"
@@ -23,50 +24,100 @@ backup_freq = 7 #days
 #Number of backups to keep (older backups will be deleted)
 number_of_backups = 3
 
+#Path to game launcher (optional)
+launcher_path = 
+
 #Neighborhoods to NOT backup (seperate with commas)
 exceptions = Tutorial`
 
+type Settings struct {
+	freq int
+	nBackups int
+	launcherPath string
+	exceptions []string
+}
+
 func main() {
-	//section: get save and backup directories
+	savePath, backupPath, err := getPaths()
+	settings := Settings{}
+	
+	if err == nil {
+		settings, err = parseSettings(backupPath)
+		
+		if err == nil {
+			err = createBackups(savePath, backupPath, settings)
+		}
+	}
+	
+	if err != nil {
+		fmt.Print("\nPress enter to start the game...")
+		fmt.Scanln()
+		fmt.Print("\n")
+	}
+	
+	err = launchGame(settings.launcherPath)
+	
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			fmt.Print("Game executable not found...")
+		} else {
+			fmt.Print(err)
+		}
+		
+		fmt.Scanln()
+		fmt.Print("\n")
+	}
+}
+
+func getPaths() (string, string, error) {
 	keys, err := registry.OpenKey(registry.LOCAL_MACHINE, "SOFTWARE\\WOW6432Node\\EA GAMES\\The Sims 2", registry.QUERY_VALUE)
 	if err != nil {
-		backupFailed("Failed to find game save location")
+		fmt.Println("Failed to find game save location")
+		return "", "", err
 	}
 	defer keys.Close()
 	
 	saveDir, _, err := keys.GetStringValue("DisplayName")
 	if err != nil {
-		backupFailed("Failed to find game save location")
+		fmt.Println("Failed to find game save location")
+		return "", "", err
 	}
 	
 	userDirPath, err := os.UserHomeDir()
 	if err != nil {
-		backupFailed("Failed to find game save location")
+		fmt.Println("Failed to find game save location")
+		return "", "", err
 	}
 	
 	savePath := filepath.Join(userDirPath, "My Documents", "EA Games", saveDir, "Neighborhoods")
 	backupPath := filepath.Join(userDirPath, "My Documents", "EA Games", "Sims 2 Backups")
 	
+	return savePath, backupPath, nil
+}
+
+func parseSettings(backupPath string) (Settings, error) {
+	settings := Settings{7, 3, "", []string{"Tutorial"}}
+	
 	buf, err := os.ReadFile(filepath.Join(backupPath, "settings.txt"))
+	
 	if errors.Is(err, fs.ErrNotExist) {
 		err := os.Mkdir(backupPath, os.ModeDir)
 		if err != nil && !errors.Is(err, fs.ErrExist) {
-			backupFailed("Failed to create the Sims 2 Backups folder")
+			fmt.Println("Failed to create the Sims 2 Backups folder")
+			return settings, err
 		}
 		
 		err = os.WriteFile(filepath.Join(backupPath, "settings.txt"), []byte(SETTINGS), 666)
 		if err != nil {
-			backupFailed("Failed to create settings.txt")
+			fmt.Println("Failed to create settings.txt")
 		}
 		
+		return settings, err
+		
 	} else if err != nil {
-		backupFailed("Failed to read settings")
+		fmt.Println("Failed to read settings")
+		return settings, err
 	}
-	
-	//section: parse settings
-	freq := 7
-	nBackups := 3
-	exceptions := []string{"Tutorial"}
 	
 	s := string(buf)
 	lines := strings.Split(s, "\n")
@@ -78,42 +129,57 @@ func main() {
 			line = line[:commentIndex]
 		}
 		
-		line = strings.TrimSpace(line)
-		line = strings.ReplaceAll(line, " ", "")
-		
 		left, right, found := strings.Cut(line, "=")
 		
 		if found {
+			left = strings.TrimSpace(left)
+			right = strings.TrimSpace(right)
+			
 			if left == "backup_freq" {
-				freq, err = strconv.Atoi(right)
+				settings.freq, err = strconv.Atoi(right)
 				
 				if err != nil {
-					backupFailed("Failed to read backup frequency")
+					fmt.Println("Failed to read backup frequency")
+					return settings, err
 				}
 				
 			} else if left == "number_of_backups" {
-				nBackups, err = strconv.Atoi(right)
+				settings.nBackups, err = strconv.Atoi(right)
 				
 				if err != nil {
-					backupFailed("Failed to read the number of backups")
+					fmt.Println("Failed to read the number of backups")
+					return settings, err
 				}
 				
+			} else if left == "launcher_path" {
+				settings.launcherPath = right
+			
 			} else if left == "exceptions" {
-				exceptions = strings.Split(right, ",")
+				settings.exceptions = strings.Split(right, ",")
 			}
 		}
 	}
 	
-	//section: loop over neighborhoods
+	return settings, nil
+}
+
+func createBackups(savePath string, backupPath string, settings Settings) error {
+	//loop over neighborhoods
 	hoodDirs, err := os.ReadDir(savePath)
 	if err != nil {
-		backupFailed("Failed to find neighborhoods")
+		fmt.Println("Failed to find neighborhoods")
+		return err
 	}
+	
+	hoodDirs = filter(hoodDirs, func(hoodDir fs.DirEntry) bool {
+		return hoodDir.IsDir() && !slices.Contains(settings.exceptions, hoodDir.Name())
+	})
 	
 	if len(hoodDirs) > 0 {
 		err := os.Mkdir(backupPath, os.ModeDir)
 		if err != nil && !errors.Is(err, fs.ErrExist) {
-			backupFailed("Could not create backups folder")
+			fmt.Println("Could not create the backups folder")
+			return err
 		}
 	}
 	
@@ -121,117 +187,100 @@ func main() {
 		hoodSavePath := filepath.Join(savePath, hoodDir.Name())
 		hoodBackupPath := filepath.Join(backupPath, hoodDir.Name())
 		
-		if hoodDir.IsDir() && !slices.Contains(exceptions, hoodDir.Name()){
-			//section: filter and sort old backups
-			err := os.Mkdir(hoodBackupPath, os.ModeDir)
-			if err != nil && !errors.Is(err, fs.ErrExist) {
-				backupFailed("Could not create the neighborhood's backup folder")
-			}
+		//filter and sort old backups
+		err := os.Mkdir(hoodBackupPath, os.ModeDir)
+		if err != nil && !errors.Is(err, fs.ErrExist) {
+			fmt.Println("Could not create the neighborhood's backup folder")
+			return err
+		}
+		
+		backups, err := os.ReadDir(hoodBackupPath)
+		if err != nil {
+			fmt.Println("Could not access the neighborhood's backups")
+			return err
+		}
+		
+		backups = filter(backups, func(backup fs.DirEntry) bool {
+			_, err := time.Parse("2006-01-02", backup.Name()[:10])
+			return err == nil && !backup.IsDir()
+		})
+		
+		slices.SortFunc(backups, func(a fs.DirEntry, b fs.DirEntry) int {
+			return -cmp.Compare(a.Name(), b.Name())
+		})
+		
+		newBackupPath := filepath.Join(hoodBackupPath, time.Now().Format("2006-01-02") + ".7z")
+		
+		//create first backup
+		if len(backups) == 0 {
+			fmt.Println("Creating Backup for " + hoodDir.Name() + "...")
+			err := createBackup(hoodSavePath, newBackupPath)
 			
-			backups, err := os.ReadDir(hoodBackupPath)
 			if err != nil {
-				backupFailed("Could not access the neighborhood's backups")
+				fmt.Println("Failed to create backup")
+				return err
 			}
 			
-			var filteredBackups []fs.DirEntry
-			
-			for _, backup := range backups {
-				_, err := time.Parse("2006-01-02", backup.Name()[:10])
-				if err == nil && !backup.IsDir() {
-					filteredBackups = append(filteredBackups, backup)
-				}
+		//create a new backup if the last backup is old
+		} else {
+			lastBackup := backups[0]
+			lastBackupDate, err := time.Parse("2006-01-02", lastBackup.Name()[:10])
+			if err != nil {
+				fmt.Println("Failed to parse backup date")
+				return err
 			}
 			
-			backups = filteredBackups
-			
-			slices.SortFunc(backups, func(a fs.DirEntry, b fs.DirEntry) int {
-				return cmp.Compare(a.Name(), b.Name())
-			})
-			
-			slices.Reverse(backups)
-			
-			//section: create backup
-			newBackupPath := filepath.Join(hoodBackupPath, time.Now().Format("2006-01-02") + ".7z")
-			
-			//create first backup
-			if len(backups) == 0 {
-				fmt.Println("Creating Backup for " + hoodDir.Name() + "...")
-				err := createBackup(hoodSavePath, newBackupPath)
-				
-				if err != nil {
-					backupFailed("Failed to create backup")
-				}
-				
-			//create new backup if the last backup is old
-			} else {
-				lastBackup := backups[0]
-				lastBackupDate, err := time.Parse("2006-01-02", lastBackup.Name()[:10])
-				if err != nil {
-					backupFailed("Failed to parse backup date")
-				}
-				
-				neighborhoodPackage := filepath.Join(hoodSavePath, hoodDir.Name() + "_Neighborhood.package")
-				info, err := os.Stat(neighborhoodPackage)
-				if err != nil {
-					backupFailed("Could not retrieve neighborhood info")
-				}
-				
-				if int(time.Since(lastBackupDate).Hours() / 24) < freq || info.ModTime().Sub(lastBackupDate).Hours() < 24 {
-					continue
-				}
-				
+			if int(time.Since(lastBackupDate).Hours() / 24) >= settings.freq {
 				fmt.Println("Creating Backup for " + hoodDir.Name() + "...")
 				err = createBackup(hoodSavePath, newBackupPath)
 				
 				if err != nil {
-					backupFailed("Failed to create backup")
+					fmt.Println("Failed to create backup")
+					return err
 				}
 				
-				//section: delete old backups
-				for i := nBackups; i < len(backups); i++ {
+				//delete old backups
+				for i := settings.nBackups; i < len(backups); i++ {
 					os.Remove(filepath.Join(hoodBackupPath, backups[i].Name()))
 				}
 			}
 		}
 	}
 	
-	//section: launch game
-	launchGame()
+	return nil
 }
 
-func backupFailed(err string) {
-	fmt.Println(err)
-	fmt.Print("\nPress enter to start the game...")
-	fmt.Scanln()
-	fmt.Print("\n")
-	launchGame()
-}
-
-func launchGame() {
-	launcherNames := [3]string{"Sims2RPC.exe", "Sims2EP9RPC.exe", "Sims2EP9.exe"}
+func launchGame(launcherPath string) error {
+	launchers := []string{}
 	
-	for _, name := range launcherNames {
-		if fileExists(name) {
-			fullPath, err := filepath.Abs(name)
-			if(err != nil) {
-				fmt.Println(err)
-			}
-			
+	if launcherPath == "" {
+		launchers = []string{"Sims2RPC.exe", "Sims2EP9RPC.exe", "Sims2EP9.exe"}
+	} else {
+		launchers = []string{launcherPath}
+	}
+	
+	for _, launcher := range launchers {
+		fullPath, err := filepath.Abs(strings.Trim(launcher, "\""))
+		if(err != nil) {
+			return err
+		}
+		
+		if fileExists(fullPath) {
 			fmt.Println("Starting Game...\n")
 			
-			cmd := exec.Command(fullPath, "")
+			cmd := exec.Command("cmd", "")
+			cmd.SysProcAttr = &syscall.SysProcAttr {
+				CmdLine: "/c start /b \"\" \"" + fullPath + "\"",
+			}
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
-			cmd.Start()
+			cmd.Run()
 			
-			os.Exit(0)
+			return nil
 		}
 	}
 	
-	fmt.Print("Game executable not found")
-	fmt.Scanln()
-	fmt.Print("\n")
-	os.Exit(1)
+	return fs.ErrNotExist
 }
 
 func createBackup(source string, destination string) error {
@@ -243,6 +292,17 @@ func createBackup(source string, destination string) error {
 	}
 	
 	return err
+}
+
+//generic filter function
+func filter[T any](slice []T, f func(T) bool) []T {
+    var n []T
+    for _, e := range slice {
+        if f(e) {
+            n = append(n, e)
+        }
+    }
+    return n
 }
 
 func fileExists(fileName string) bool {
--- build.bat
+++ build.bat
@@ -4,4 +4,5 @@ mkdir build build\TSBin "build\Sims 2 Backups"
 move Sims2Backup.exe build\TSBin\Sims2Backup.exe
 copy 7zr.exe build\TSBin\7zr.exe
 copy settings.txt "build\Sims 2 Backups\settings.txt"
+powershell Compress-Archive -Force -Path "build\Sims` 2` Backups", build\TSBin -Destination build\Sims2BackupLauncher.zip
 pause
